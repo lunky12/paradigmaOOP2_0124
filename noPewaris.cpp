76b1612de3bc7035e5fdd18e52836ae5e8a10c41
lunky12 <lunkyumy@gmail.com>
2026-05-19 18:59:08 +0700

    class derived class dan int main no pewaris

--- noPewaris.cpp
+++ noPewaris.cpp
@@ -13,3 +13,15 @@ public:
         cout << "Hallo saya Function dari base class";
     }
 };
+class derivedClass : public baseClass1 {
+public:
+    void perkenalan() {
+        cout << "Hallo saya Function dari derived Class";
+    }
+};
+int main() {
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
